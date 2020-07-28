5e2836ab2412a2d923654864c8d85806ddc5d6e9
Thiago Lima <thiso2@hotmail.com>
2020-07-28 20:35:03 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,11 +3,12 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry, tap } from 'rxjs/operators';
 import { StorageService } from 'src/services/storage.service';
+import { AlertController } from '@ionic/angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -30,9 +31,16 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status) {
+                    case 401:
+                    this.handle401();
+                    break;
+
                     case 403:
                     this.handle403();
                     break;
+
+                    default:
+                    this.handleDefaultError(errorObj);
                 }
 
                 return throwError(errorObj);
@@ -42,6 +50,36 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    async handle401() {
+        const alert = await this.alertCtrl.create({
+            //cssClass: 'my-custom-class',
+            header: 'Erro 401: falha de autenticação',
+            //subHeader: 'Subtitle',
+            message: 'Email ou senha incorretos',
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        await alert.present();
+    }
+
+    async handleDefaultError(errorObj) {
+        const alert = await this.alertCtrl.create({
+            //cssClass: 'my-custom-class',
+            header: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            //subHeader: 'Subtitle',
+            message: errorObj.message,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        await alert.present();
     }
 }
 
