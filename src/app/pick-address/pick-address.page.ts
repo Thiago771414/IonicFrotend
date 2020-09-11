82f8684f1a39c517a35fafbf4b4d9544bf5bdfba
Thiago Lima <thiso2@hotmail.com>
2020-09-11 13:08:29 -0300

    Buscando enderecos do banco de dados

--- src/app/pick-address/pick-address.page.ts
+++ src/app/pick-address/pick-address.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EnderecoDTO } from 'src/models/endereco.dto';
+import { StorageService } from 'src/services/storage.service';
+import { ClienteService } from 'src/services/domain/cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pick-address',
@@ -10,43 +13,23 @@ export class PickAddressPage implements OnInit {
 
   items: EnderecoDTO[];
 
-  constructor() { }
+  constructor(public storage: StorageService, public clienteService: ClienteService, private router: Router) { }
 
   ngOnInit() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 200",
-        bairro: "Santa Mônica",
-        cep: "48293822",
-        cidade: {
-          id: "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
-          }
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email){
+       this.clienteService.findByEmail(localUser.email)
+       .subscribe(response => {
+        this.items = response['enderecos'];
+       },
+       error => {
+         if(error.status == 403) {
+          this.router.navigate(['folder/:id']);
         }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Alexandre Toledo da Silva",
-        numero: "405",
-        complemento: null,
-        bairro: "Centro",
-        cep: "88933822",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
-          }
-        }
-      }
-    ];
+      });
+    }
+    else {
+      this.router.navigate(['folder/:id']);
+    }
   }
-
 }
